4676277cc13e90ff5141608f37652c85bdf1cc3e
xProsper <xprosper.play@gmail.com>
2021-12-01 14:25:31 -0500

    dynamic creation of DTS files

--- particubes.js
+++ particubes.js
@@ -1,4 +1,29 @@
-import { readdirSync, readFileSync } from "fs";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
 
-export default readdirSync("./resources").map((file) => JSON.parse(readFileSync(path.join("./resources", file), "utf8")));
+const getTypeTemplate = r =>`declare namespace ${r.type} {
+
+}`;
+
+const getIndexTemplate = r => `
+${getModules(r).join("\n")}
+`;
+
+const getModules = ({ blocks }) => blocks
+  .filter(block => Object.keys(block).includes("list"))[0]
+  .list.map(item => `/// <reference path="./${item.replace(/<[^>]+>/gi, "")}.d.ts" />`);
+
+readdirSync("./resources").forEach(file => {
+  const r = JSON
+    .parse(readFileSync(path.join("./resources", file), "utf8")
+    .replace(/built-ins/gi, "builtIns"));
+
+  const typeTemplate = getTypeTemplate(r);
+
+  if (!r.type) {
+    writeFileSync(`./types/index.d.ts`, getIndexTemplate(r));
+  } else {
+    if (r.type.substring(0, 1) === r.type.substring(0, 1).toUpperCase())
+      writeFileSync(`./types/${r.type}.d.ts`, typeTemplate);
+  };
+});
